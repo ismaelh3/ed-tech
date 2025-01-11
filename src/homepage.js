cdd45f6fc597171682f9ebf11daffa98eecce3dc
Ismael Hernandez <iher4987@gmail.com>
2025-01-11 14:39:27 -0600

    making more changes

--- src/homepage.js
+++ src/homepage.js
@@ -2,15 +2,21 @@ import './homepage.css';
 
 function NavigatonBar() {
     return (
-        <div className = "NavigationBar">   
-            <a className = "options" href = "#road"> Roadmap </a>
-            <a href = "#explore"> Explore </a>
-            <a href = "#resources" > Resources </a>
-            <img src = "https://banner2.cleanpng.com/20180503/uaq/avdoc9im6.webp" alt = "logo" className = "templogo"/>
-            <a href = "#about" > About </a>
-            <a href = "#signin"> Sign In </a>
-            <input className = "searchbar" placeholder = 'Search...'/>
+    <div className = 'NavigationBar'>
+        <div className = 'FirstThree'>   
+            <a href = "#road"> Roadmap </a>
+            <a href = "#Explore"> Explore </a>
+            <a href = "#Resources" > Resources </a>
         </div> 
+        <div className = "Middle"> 
+            <img src = "https://banner2.cleanpng.com/20180503/uaq/avdoc9im6.webp" alt = "logo" className = "TempLogo"/> 
+        </div>
+        <div className = "LastThree"> 
+            <a href = "#About" > About </a>      
+            <input className = "SearchBar" placeholder = 'Search...'/>
+            <a href = "#SignIn"> Sign In </a>
+        </div> 
+    </div>
     );
 }
 
