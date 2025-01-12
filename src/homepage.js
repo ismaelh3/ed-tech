2216f79d5ee88d4a6db69a2e3609d7cce10cab96
Ismael Hernandez <iher4987@gmail.com>
2025-01-11 19:51:57 -0600

    made some changes

--- src/homepage.js
+++ src/homepage.js
@@ -3,17 +3,17 @@ import './homepage.css';
 function NavigatonBar() {
     return (
     <div className = 'NavigationBar'>
-        <div className = 'FirstThree'>   
+        <div className = 'FirstDiv'>   
             <a href = "#road"> Roadmap </a>
             <a href = "#Explore"> Explore </a>
             <a href = "#Resources" > Resources </a>
+            <a href = "#About" > About </a>      
         </div> 
-        <div className = "Middle"> 
+        <div className = "MiddleDiv"> 
             <img src = "https://banner2.cleanpng.com/20180503/uaq/avdoc9im6.webp" alt = "logo" className = "TempLogo"/> 
         </div>
-        <div className = "LastThree"> 
-            <a href = "#About" > About </a>      
-            <input className = "SearchBar" placeholder = 'Search...'/>
+        <div className = "ThirdDiv"> 
+            <input className = "SearchBar" type = 'text' placeholder = 'Search...'/>
             <a href = "#SignIn"> Sign In </a>
         </div> 
     </div>
